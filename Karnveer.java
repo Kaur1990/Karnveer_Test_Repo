1bc3e55882199533e846e63540516067846d020d
Kaur1990 <kapoor.raman1990@gmail.com>
2024-01-12 13:58:23 +0530

    for testing purpose

---
+++ Karnveer.java
@@ -0,0 +1,20 @@
+public class Karnveer
+{
+
+public static void main(String args[])
+{
+
+char ch='\u0A15';
+char ch1='\u0A30';
+char ch2='\u0A28';
+char ch3='\u0A35';
+char ch4='\u0A40';
+char ch5='\u0A30';
+char ch6='\u0A3F';
+char ch7='\u0A70';
+char ch8='\u0A18';
+
+System.out.println(ch+""+ch1+""+ch2+""+ch3+""+ch4+""+ch5+""+ch6+""+ch7+""+ch8);
+
+}
+}
